cc6a6748eb04a8d0bb918e1d206a6954ecae9609
Oleg Liepkalo <liepkalo.o@gmail.com>
2019-11-25 22:23:43 +0200

    Updates

---
+++ precache-manifest.b5c45913dff68f5781226a94d41a3788.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5617eb68456069026b66fd9c94260c78",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "27151ee6192083484468",
+    "url": "/goit-react-hw-03-image-finder/static/css/2.292b22a7.chunk.css"
+  },
+  {
+    "revision": "ee8377837ad42e3d1155",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.2a63f3eb.chunk.css"
+  },
+  {
+    "revision": "27151ee6192083484468",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.3b022ca2.chunk.js"
+  },
+  {
+    "revision": "ee8377837ad42e3d1155",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.412a3089.chunk.js"
+  },
+  {
+    "revision": "f54717b5819d88fe3b6e",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.918547ac.js"
+  }
+]);
